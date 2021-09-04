3da3bc4ac1d642ccd21060f058d63b21cac0505b
Wither_87 <wither_87@mail.ru>
2021-09-04 16:21:54 +0500

    Создание нового поста

--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -9,11 +9,18 @@ import java.util.List;
 
 @Service
 public class PostService {
+    private List<Post> posts;
+    public PostService(){
+        posts = new ArrayList<>();
+        posts.add(new Post("Никита Хотдог", new Date()));
+        posts.add(new Post("Никита Шаурма", new Date()));
+        posts.add(new Post("Никита", new Date()));
+    }
     public List<Post> listAllPosts(){
-        List<Post> list = new ArrayList<>();
-        list.add(new Post("Никита Хотдог", new Date()));
-        list.add(new Post("Никита Шаурма", new Date()));
-        list.add(new Post("Никита", new Date()));
-        return list;
+        return posts;
+    }
+
+    public void create(String text){
+        posts.add(new Post(text, new Date()));
     }
 }
